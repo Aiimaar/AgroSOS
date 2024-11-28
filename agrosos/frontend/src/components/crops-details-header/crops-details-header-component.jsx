689a248a0e3850e5aa03c316fa44355d7a3bfe2d
Victor <victorfelipesuarez@alumno.ieselrincon.es>
2024-11-28 18:53:05 +0000

    fixed bugs

--- agrosos/frontend/src/components/crops-details-header/crops-details-header-component.jsx
+++ agrosos/frontend/src/components/crops-details-header/crops-details-header-component.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom"; // Importa useNavigate
 import graficaAjo from "./images/grafica-ajo.png";
 import ajo from "./images/ajo.png";
 import "./crops-details-header-component.css";
 
 const CropsDetailsHeaderComponent = ({ cropImage = ajo, onBack }) => {
+  const navigate = useNavigate(); // Define navigate
+
   return (
     <div className="crops-header">
-      <button className="back-button" />
+      <button
+        className="crop-details-back-button"
+        onClick={onBack || (() => navigate("/advices"))} // Usa onBack si está definido, de lo contrario navigate
+      >
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </button>
       <div className="crop-info">
         <div className="crop-image-container">
-          <img src={ajo} alt="Crop" className="crop-image" />
+          <img src={cropImage} alt="Crop" className="crop-image" /> {/* Usa cropImage dinámicamente */}
         </div>
         <h1 className="crop-title">AJO</h1>
         <div className="chart-image-container">
