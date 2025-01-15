17a0924a8e0393e488b6629faa4959154f5b7f1e
Aiimaar <aimar.3deso@gmail.com>
2025-01-15 17:34:18 +0000

    Fixing conflicts

--- agrosos/frontend/src/pages/plot-list/PlotList.jsx
+++ agrosos/frontend/src/pages/plot-list/PlotList.jsx
@@ -1,7 +1,6 @@
 import PlotListComp from "../../components/plot-list-comp/plot-list-comp";
 import Header from "../../components/header/header";
 import PlotListFooterComp from "../../components/plot-list-footer-comp/plot-list-footer-comp";
-import ViewHeaderComponent from "../../components/view-header/view-header";
 
 function PlotList(){
     return (
