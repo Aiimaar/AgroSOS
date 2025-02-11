29399df1e61c4996b54e328f086ed36a5bf1fbcb
Victor <victorfelipesuarez@alumno.ieselrincon.es>
2025-02-11 16:25:16 +0000

    local changes

--- agrosos/frontend/src/services/subscriptionService.js
+++ agrosos/frontend/src/services/subscriptionService.js
@@ -3,13 +3,6 @@ import axios from 'axios';
 // const API = "http://localhost:8080/api/subscriptions";
 const API = process.env.REACT_APP_API_URL;
 
-function unregisterAllServiceWorkers() {
-  navigator.serviceWorker.getRegistrations().then(function (registrations) {
-    for (let registration of registrations) {
-      registration.unregister();
-    }
-  });
-}
 
 async function regSw() {
   if ('serviceWorker' in navigator) {
@@ -33,49 +26,7 @@ async function subscribe(serviceWorkerReg, subscriptionName) {
   }
 }
 
-async function sendNotificationToSubscriptionName(subscriptionName, notificationMessage) {
-  const message = {
-    subscriptionName,
-    notificationMessage
-  }
-  return axios.post(`${API}/sendNotificationToSubscriptionName`, message);
-}
-
-async function getAllSubscriptions() {
-  return axios.get(`${API}`);
-}
-
-async function checkIfAlreadySubscribed() {
-  const serviceWorkerReg = await navigator.serviceWorker.getRegistration('/sw.js');
-  if (!serviceWorkerReg) return false;
-
-  let subscription = await serviceWorkerReg.pushManager.getSubscription();
-
-  if (subscription !== null) return true;
-
-  return false;
-}
-
-async function unregisterFromServiceWorker() {
-  const serviceWorkerReg = await navigator.serviceWorker.getRegistration('/sw.js');
-
-  if (!serviceWorkerReg) return;
-  let subscription = await serviceWorkerReg.pushManager.getSubscription();
-
-  if (!subscription) return;
-
-  // I use the endpoint to delete a subscription. 
-  // I use a non standard POST to delete the subscription in Backend
-  await axios.post(`${API}/deleteByEndpoint`, { endpoint: subscription.endpoint });
-  await subscription.unsubscribe();
-}
-
 export {
   regSw,
   subscribe,
-  unregisterAllServiceWorkers,
-  checkIfAlreadySubscribed,
-  getAllSubscriptions,
-  sendNotificationToSubscriptionName,
-  unregisterFromServiceWorker
 };
